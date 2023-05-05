a095311f1ab4e2a1f2809b4bd4c8f667ce552ce1
Lucas F. Albuquerque <90412000+lucasfalb@users.noreply.github.com>
2023-05-05 11:35:03 -0300

    atualizando server

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,19 @@
 import { Router } from 'express'
+import auth from './middlewares/auth'
+import SessionsController from './controllers/SessionsController'
 import HelloController from './controllers/HelloController'
 import UsersController from './controllers/UsersController'
 const routes = new Router()
 
 routes.get('/hello', HelloController.index)
+
+// ROUTER AUTHENTICATION
+routes.post('/sessions', SessionsController.create)
+// POST USER - CREATE USER
+routes.post('/users', UsersController.create)
+
+// MIDDLEWARE PRIVATE ROUTES 
+routes.use(auth)
 
 // GET ALL USERS
 routes.get('/users', UsersController.index)
@@ -11,8 +21,6 @@ routes.get('/users', UsersController.index)
 // GET USER INFO
 routes.get('/users/:id', UsersController.show)
 
-// POST USER - CREATE USER
-routes.post('/users', UsersController.create)
 
 // PUT USER INFO - UPDATE USER
 routes.put('/users/:id', UsersController.update)
--- src/services/auth.js
+++ src/services/auth.js
@@ -3,4 +3,10 @@ import bcrypt from "bcryptjs"
 export const createPasswordHash = async (password) => {
     const hashedPassword = await bcrypt.hash(password, 8);
     return hashedPassword;
- }
+}
+
+export const checkPassword = async (user, password) => {
+    const match = await bcrypt.compare(password, user.password);
+    return match;
+}
+
